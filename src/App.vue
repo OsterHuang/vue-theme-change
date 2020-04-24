889903f9ac26fd7f9490fc1210a11470d6879000
ST_Oster.Huang <oster.huang@solartninc.com>
2020-04-24 15:53:36 +0800

    變色實作嘍

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,73 @@
 <template>
-  <div id="app">
+  <div id="app" :class="themeName">
+    <button @click="changeLightColor">淺色</button>
+    <button @click="changeDarkColor">深色</button>
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <div class="root">
+      我是誰？
+      <div class="root-title">
+        化文與組織
+      </div>
+      <div class="root-content">
+        今天有幾個人在這邊加班的嗎？
+        <div class="remark">
+          Javascript, Java, SCSS, SASS, stylus, 
+        </div>
+        別再加班啦，趕快下班洗洗睡…
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      themeName: 'theme-light'
+    }
+  },
+  methods: {
+    changeLightColor() {
+      this.themeName = 'theme-light'
+    },
+    changeDarkColor() {
+      this.themeName = 'theme-dark'
+    }
   }
 }
 </script>
 
-<style>
+<style lang="scss">
+@import "./styles/theme/light.scss";
+@import "./styles/theme/dark.scss";
+
+.root {
+  padding: 16px;
+  border: 1px solid var(--main-color-1);
+  background-color: var(--main-color-2);
+  color: var(--secondary-color-1);
+
+  .root-title {
+    font-size: 36px;
+    font-weight: bold;
+    color: var(--secondary-color-3);
+  }
+
+  .root-content {
+    padding: 24px;
+    border-top: 1px solid var(--primary-color);
+    font-size: 14px;
+
+    .remark {
+      margin: 10px 0px;
+      padding: 20px;
+      background-color: var(--main-color-3);
+    }
+  }
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/styles/smbf-sample-color-system.js
@@ -0,0 +1,47 @@
+export default {
+  colors: {
+    // ----
+    // 以下色组 - 由designer提供
+    // ----
+    primary: {
+      default: '#7B38D1',
+      dark: '#311179',
+      light: '#A266E3',
+      lighter: '#b68bec'
+    },
+    secondary: {
+      default: '#699A04',
+      light: '#a2c04e'
+    },
+    tertiary: {
+      default: '#dc954a'
+    },
+    alert: {
+      default: '#D10000'
+    },
+    neutral: {
+      default: '#A59FAC',
+      light: '#D8D8D8',
+      lighter: '#EEEEEE',
+      dark: '#6F6A76'
+    },
+    primaryBg: {
+      color: '#ffffff',
+      foreground: '#000000'
+    },
+    secondaryBg: {
+      color: '#343434',
+      foreground: '#ffffff'
+    },
+    gradient: {
+      // 頁面Banner
+      live: ['#7B38D1', '#D16938'],
+      // 大家都關注的牌卡「直播中」左上彩帶
+      label: ['#699A04', '#98C234'],
+      // 未開賽賽事牌卡背景色
+      notStarted: ['#562385', '#005EAB'],
+      // 已結束賽事牌卡背景色
+      finished: ['#000000', '#6F6A76']
+    }
+  }
+}
